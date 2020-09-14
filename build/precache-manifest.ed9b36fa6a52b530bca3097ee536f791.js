1c50629d30854f5eebfa07cef0eee85005ea795b
Dugald Morrow <dmorrow@atlassian.com>
2020-09-14 14:28:56 +1000

    Fixes

---
+++ build/precache-manifest.ed9b36fa6a52b530bca3097ee536f791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13801176268e899d84f3a8a48b95881d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010492da2d49c156e9eb",
+    "url": "/static/js/2.82e2734a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.82e2734a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f1804a1391b3c72b25",
+    "url": "/static/js/main.3335ff7a.chunk.js"
+  },
+  {
+    "revision": "3bdedc1ee73ed71729f7",
+    "url": "/static/js/runtime-main.64cf511c.js"
+  }
+]);
